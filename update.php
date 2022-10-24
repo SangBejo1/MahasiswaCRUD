13d4c8517d488b9bbb82ff4c18ab079828e50b55
SangBejo1 <ayubsubagiya@gmail.com>
2022-10-25 00:04:40 +0700

    MahasiswaCRUD

--- update.php
+++ update.php
@@ -3,89 +3,97 @@
 require_once "config.php";
  
 // Define variables and initialize with empty values
-$Nama = $NIM = $Tugas = $UTS = $UAS ="";
-$Nama_err = $NIM_err = $Tugas_err = $UTS_err = $UAS_err ="";
+$NIM = $Nama =  $Tugas =$UTS =$UAS ="";
+$NIM_err = $Nama_err  =$Tugas_err =$UTS_err =$UAS_err ="";
+
  
 // Processing form data when form is submitted
-if($_SERVER["REQUEST_METHOD"] == "POST"){
-    // Validate name
-    $input_name = trim($_POST["Nama"]);
-    if(empty($input_name)){
-        $Nama_err = "Please enter a name.";
-    } elseif(!filter_var($input_name, FILTER_VALIDATE_REGEXP, array("options"=>array("regexp"=>"/^[a-zA-Z\s]+$/")))){
-        $Nama_err = "Please enter a valid name.";
-    } else{
-        $Nama = $input_name;
-    }
+if(isset($_POST["id"]) && !empty($_POST["id"])){
+    // Get hidden input value
+    $id = $_POST["id"];
     
-    // Validate address
+    // Validate nim
     $input_NIM = trim($_POST["NIM"]);
     if(empty($input_NIM)){
-        $NIM_err = "Please enter the NIM.";     
+        $NIM_err = "Please enter the nim value.";
     } elseif(!ctype_digit($input_NIM)){
         $NIM_err = "Please enter a positive integer value.";
     } else{
         $NIM = $input_NIM;
     }
-    
-    // Validate salary
-    $input_Tugas = trim($_POST["Tugas"]);
-    if(empty($input_Tugas)){
-        $Tugas_err = "Please enter the Score.";     
-    } elseif(!ctype_digit($input_Tugas)){
-        $Tugas_err = "Please enter a positive integer value.";
-    } else{
-        $Tugas = $input_Tugas;
-    }
 
-    $input_UTS = trim($_POST["UTS"]);
-    if(empty($input_UTS)){
-        $UTS_err = "Please enter the Score.";     
-    } elseif(!ctype_digit($input_UTS)){
-        $UTS_err = "Please enter a positive integer value.";
-    } else{
-        $UTS = $input_UTS;
-    }
+    // Validate name
+  $input_Nama = trim($_POST["Nama"]);
+  if(empty($input_Nama)){
+      $Nama_err = "Please enter a name.";
+  } elseif(!filter_var($input_Nama, FILTER_VALIDATE_REGEXP, array("options"=>array("regexp"=>"/^[a-zA-Z\s]+$/")))){
+      $Nama_err = "Please enter a valid name.";
+  } else{
+      $Nama = $input_Nama;
+  }
 
-    $input_UAS = trim($_POST["UAS"]);
-    if(empty($input_UAS)){
-        $UAS_err = "Please enter the Score.";     
-    } elseif(!ctype_digit($input_UAS)){
-        $UAS_err = "Please enter a positive integer value.";
-    } else{
-        $UAS = $input_UAS;
-    }
-    
+  
+  // Validate tugas
+  $input_Tugas = trim($_POST["Tugas"]);
+  if(empty($input_Tugas)){
+      $Tugas_err = "Please enter the tugas value.";
+  } elseif(!ctype_digit($input_Tugas)){
+      $Tugas_err = "Please enter a positive integer value.";
+  } else{
+      $Tugas = $input_Tugas;
+  }
+
+  // Validate uts
+  $input_UTS = trim($_POST["UTS"]);
+  if(empty($input_UTS)){
+      $UTS_err = "Please enter the uts value.";
+  } elseif(!ctype_digit($input_UTS)){
+      $UTS_err = "Please enter a positive integer value.";
+  } else{
+      $UTS = $input_UTS;
+  }
+
+  // Validate uas
+  $input_UAS = trim($_POST["UAS"]);
+  if(empty($input_UAS)){
+      $UAS_err = "Please enter the uas value.";
+  } elseif(!ctype_digit($input_UAS)){
+      $UAS_err = "Please enter a positive integer value.";
+  } else{
+      $UAS = $input_UAS;
+  }
+
     // Check input errors before inserting in database
-    if(empty($Nama_err) && empty($NIM_err) && empty($Tugas_err)&& empty($UTS_err)&& empty($UAS_err)){
-        // Prepare an insert statement
-        $sql = "UPDATE mahasiswa SET Nama=?, NIM=?, Tugas=?, UTS=?, UAS=? WHERE id=?";
-         
+    if(empty($NIM_err) && empty($Nama_err) && empty($Tugas_err)  && empty($UTS_err)  && empty($UAS_err)){
+        // Prepare an update statement
+        $sql = "UPDATE mahasiswa SET NIM=?, Nama=?, Tugas=?, UTS=?, UAS=? WHERE id=?";
+
         if($stmt = mysqli_prepare($link, $sql)){
             // Bind variables to the prepared statement as parameters
-            mysqli_stmt_bind_param($stmt, "sssssi", $param_Nama, $param_NIM, $param_Tugas, $param_UTS, $param_UAS, $param_id);
-            
+            mysqli_stmt_bind_param($stmt, "sssssi",$param_NIM, $param_Nama,  $param_Tugas,  $param_UTS,  $param_UAS, $param_id);
+
             // Set parameters
-            $param_Nama = $Nama;
             $param_NIM = $NIM;
-            $param_Tugas = $Tugas;
+            $param_Nama = $Nama;
+            $param_Tugas =  $Tugas;
             $param_UTS = $UTS;
             $param_UAS = $UAS;
             $param_id = $id;
+
             // Attempt to execute the prepared statement
             if(mysqli_stmt_execute($stmt)){
                 // Records created successfully. Redirect to landing page
                 header("location: index.php");
                 exit();
             } else{
-                echo "Oops! Something went wrong. Please try again later.";
+                echo "Something went wrong. Please try again later.";
             }
         }
-         
+
         // Close statement
         mysqli_stmt_close($stmt);
     }
-    
+
     // Close connection
     mysqli_close($link);
 } else{
@@ -113,16 +121,13 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                     $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
                     
                     // Retrieve individual field value
-                $Nama = $row["Nama"];
-                $NIM = $row["NIM"];
-                $Tugas = $row["Tugas"];
-                $UTS = $row["UTS"];
-                $UAS = $row["UAS"];
-                } else{
-                    // URL doesn't contain valid id. Redirect to error page
-                    header("location: error.php");
-                    exit();
-                }
+                    $NIM = $row["NIM"];
+                    $Nama = $row["Nama"];
+                    $Tugas = $row["Tugas"];
+                    $UTS = $row["UTS"];
+                    $UAS = $row["UAS"];
+                   
+                } 
                 
             } else{
                 echo "Oops! Something went wrong. Please try again later.";
@@ -134,12 +139,9 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
         
         // Close connection
         mysqli_close($link);
-    }  else{
-        // URL doesn't contain id parameter. Redirect to error page
-        header("location: error.php");
-        exit();
-    }
+    }  
 }
+
 ?>
  
 <!DOCTYPE html>
@@ -147,10 +149,10 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 <head>
     <meta charset="UTF-8">
     <title>Update Record</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <style>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+    <style type="text/css">
         .wrapper{
-            width: 1080px;
+            width: 500px;
             margin: 0 auto;
         }
     </style>
@@ -160,36 +162,39 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
         <div class="container-fluid">
             <div class="row">
                 <div class="col-md-12">
-                    <h2 class="mt-5">Update Record</h2>
-                    <p>Please edit the input values and submit to update the mahasiswa record.</p>
+                    <div class="page-header">
+                        <h2>Update Record</h2>
+                    </div>
+                    <p>Please edit the input values and submit to update the record.</p>
                     <form action="<?php echo htmlspecialchars(basename($_SERVER['REQUEST_URI'])); ?>" method="post">
-                    <div class="form-group">
-                            <label>Name</label>
-                            <input type="text" name="Nama" class="form-control <?php echo (!empty($Nama_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $Nama; ?>">
-                            <span class="invalid-feedback"><?php echo $Nama_err;?></span>
-                        </div>
-                        <div class="form-group">
-                            <label>NIM</label>
-                            <input type="text" name="NIM" class="form-control <?php echo (!empty($NIM_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $NIM; ?>">
-                            <span class="invalid-feedback"><?php echo $NIM_err;?></span>
-                        </div>
-                        <div class="form-group">
-                            <label>Tugas</label>
-                            <input type="text" name="Tugas" class="form-control <?php echo (!empty($Tugas_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $Tugas; ?>">
-                            <span class="invalid-feedback"><?php echo $Tugas_err;?></span>
-                        </div>
-                        <div class="form-group">
-                            <label>UTS</label>
-                            <input type="text" name="UTS" class="form-control <?php echo (!empty($UTS_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $UTS; ?>">
-                            <span class="invalid-feedback"><?php echo $UTS_err;?></span>
-                        </div>
-                        <div class="form-group">
-                            <label>UAS</label>
-                            <input type="text" name="UAS" class="form-control <?php echo (!empty($UAS_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $UAS; ?>">
-                            <span class="invalid-feedback"><?php echo $UAS_err;?></span>
-                        </div>
+                    <div class="form-group <?php echo (!empty($NIM_err)) ? 'has-error' : ''; ?>">
+                                <label>NIM</label>
+                                <input type="text" name="NIM" class="form-control" value="<?php echo $NIM; ?>">
+                                <span class="help-block"><?php echo $NIM_err;?></span>
+                            </div>
+                            <div class="form-group <?php echo (!empty($Nama_err)) ? 'has-error' : ''; ?>">
+                                <label>Nama</label>
+                                <input type="text" name="Nama" class="form-control" value="<?php echo $Nama; ?>">
+                                <span class="help-block"><?php echo $Nama_err;?></span>
+                            </div>
+                            <div class="form-group <?php echo (!empty($Tugas_err)) ? 'has-error' : ''; ?>">
+                                <label>Tugas</label>
+                                <input type="text" name="Tugas" class="form-control" value="<?php echo $Tugas; ?>">
+                                <span class="help-block"><?php echo $Tugas_err;?></span>
+                            </div>
+                            <div class="form-group <?php echo (!empty($UTS_err)) ? 'has-error' : ''; ?>">
+                                <label>UTS</label>
+                                <input type="text" name="UTS" class="form-control" value="<?php echo $UTS; ?>">
+                                <span class="help-block"><?php echo $UTS_err;?></span>
+                            </div>
+                            <div class="form-group <?php echo (!empty($UAS_err)) ? 'has-error' : ''; ?>">
+                                <label>UAS</label>
+                                <input type="text" name="UAS" class="form-control" value="<?php echo $UAS; ?>">
+                                <span class="help-block"><?php echo $UAS_err;?></span>
+                            </div>
+                        <input type="hidden" name="id" value="<?php echo $id; ?>"/>
                         <input type="submit" class="btn btn-primary" value="Submit">
-                        <a href="index.php" class="btn btn-secondary ml-2">Cancel</a>
+                        <a href="index.php" class="btn btn-default">Cancel</a>
                     </form>
                 </div>
             </div>        
